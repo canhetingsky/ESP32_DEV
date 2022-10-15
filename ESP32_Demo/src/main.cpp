19aa202cac2b7f7c859e7a2025c18885cd2f6c2b
canhetingsky <2380348167@qq.com>
2022-10-15 17:14:27 +0800

    feat: 🎸 ESP32 demo example with VSCode and PlatformIO

---
+++ ESP32_Demo/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+void setup()
+{
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+}
+
+void loop()
+{
+    // put your main code here, to run repeatedly:
+    Serial.println("hello world!");
+    delay(1000);
+}
